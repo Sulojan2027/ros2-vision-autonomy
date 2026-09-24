d2957e5a2577842ad3e5981ad0c81f07cd48fa63
Sulojan <sulojanrajkumar@gmail.com>
2026-09-24 17:34:13 +0530

    feat: Added params fallback

--- src/my_first_pkg/my_first_pkg/simple_publisher.py
+++ src/my_first_pkg/my_first_pkg/simple_publisher.py
@@ -1,23 +1,51 @@
 import rclpy
 from rclpy.node import Node
 from rclpy.executors import ExternalShutdownException
+from rcl_interfaces.msg import SetParametersResult, FloatingPointRange, ParameterDescriptor
 from std_msgs.msg import String
 
 class SimplePublisher(Node):
     def __init__(self):
         super().__init__("my_publisher")
+        pubrate_range = FloatingPointRange(
+            from_value=0.1,
+            to_value=50.0,
+            step=0.0
+        )
+        param_descriptor = ParameterDescriptor(
+            description="Message Publishing rate restricted between 0.1 and 50.0",
+            floating_point_range=[pubrate_range]
+        )
+        self.declare_parameter("message_prefix", "Hey buddy!")
+        self.declare_parameter(
+            "publish_rate", 2.0, param_descriptor
+        )
+        
         self.publisher_ = self.create_publisher(
             String,"greetings",10
         )
         self.timer_=self.create_timer(
-            timer_period_sec=0.5,
+            timer_period_sec=1/self.get_parameter("publish_rate").value,
             callback=self.timer_callback
         )
         self.counter = 0
-        
+
+        self.add_post_set_parameters_callback(self.apply_params)
+    
+    def apply_params(self, params):
+        for p in params:
+            if p.name == "publish_rate":
+                self.destroy_timer(self.timer_)
+                self.timer_ = self.create_timer(
+                    timer_period_sec= 1 / p.value,
+                    callback=self.timer_callback
+                )
+                self.get_logger().info(f"Publish rate changed to {p.value} Hz")
+
     def timer_callback(self):
+        msg_prefix = self.get_parameter("message_prefix").value
         msg = String()
-        msg.data = f"Hey buddy! {self.counter}"
+        msg.data = f"{msg_prefix} {self.counter}"
         self.publisher_.publish(msg)
         self.get_logger().info(f"Publishing {msg.data}....!")
         self.counter += 1
@@ -28,7 +56,7 @@ def main(args=None):
     try:  
         rclpy.spin(publisher_node)
     except (KeyboardInterrupt, ExternalShutdownException):
-        publisher_node.get_logger().info("Shutdown externally...")
+        print("Shutdown externally...")
     finally:
         publisher_node.destroy_node()
         rclpy.try_shutdown()
